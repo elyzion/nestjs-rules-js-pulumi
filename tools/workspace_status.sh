b606e2fc80b584cee215c7d706ff85ce462e2cad
Berthold Alheit <elyzion@users.noreply.github.com>
2023-04-24 12:46:39 +0900

    feat: add stamping and tagging of images
    This commit enables basic git backed, workspace status.
    
    Workspace status is also used for tagging of images.
    
    We use the `.bazelrc` file to automate stamping when executing
    `bazel run`.

---
+++ tools/workspace_status.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# See https://bazel.build/docs/user-manual#workspace-status
+
+TAG=`git describe --long 2> /dev/null | sed -e 's/^v//;s/-/./;s/-g/+/'`
+LOCAL_CHANGES=`git status --untracked-files=no --porcelain`
+
+# Starts with the STABLE_ prefix so that Bazel will always rebuild stamped outputs if the git tag changes.
+echo "STABLE_BUILD_VERSION ${TAG:-NONE}$(if [[ $LOCAL_CHANGES == "" ]]; then echo ''; else echo '.with-local-changes'; fi)"
+echo "STABLE_GIT_COMMIT $(git rev-parse HEAD)"
+echo "STABLE_USER_NAME $USER"
+echo "BUILD_SCM_USER $(git config user.email)"
+echo "BUILD_SCM_BRANCH $(git symbolic-ref --short HEAD)"
+echo "BUILD_CURRENT_TIME $(date +%s)"
+echo "BUILD_RANDOM_HASH $(cat /proc/sys/kernel/random/uuid)"
